4b2e1c4dda032dc57047f461ad0c233ffba559b3
Lyssa Li <lyssa.li137@gmail.com>
2024-11-28 13:12:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# unfinished game project
+
+hello hello, this is an unfinished game project
+
+- to insert dialogue please refer to data.json for reference
+- there will be modifers for choices and links added later...
+
+list of to-dos for coding:
+
+- [ ] minigame support
+- [ ] support for modifiers
+  - per line basis modifiers
+    - timer delay
+    - color (HEX)
+    - size
+  - per scene basis modifers
+    - change text size starting from x-dialogue
+    - glitch
+    - SET game settings / IF game settings
+- italics and bold with __ and **
---
+++ src/game.js
@@ -0,0 +1,70 @@
+let GAME_SCRIPT = {};
+let GAME_SETTINGS = {
+  savedNames: {
+  },
+};
+
+const d_div = document.getElementById("narrator-dialogue");
+const m_div = document.getElementById("player-task");
+const c_div = document.getElementById("player-choices");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  loadGameSave();
+  const res = await fetch('./src/data.json');
+  startGame(JSON.parse(await res.text()));
+});
+
+function loadGameSave() {
+  // load in local stoage data
+}
+
+function startGame(script) {
+  GAME_SCRIPT = script;
+  loadScene(Object.keys(GAME_SCRIPT)[0]);
+}
+
+function loadScene(key) {
+  clearScreen();
+  GAME_SCRIPT[key].dialogue.forEach(dialogue => {
+    d_div.append(createDialogue(dialogue));
+  });
+
+  GAME_SCRIPT[key].choices.forEach(choice => {
+    c_div.append(createChoice(choice));
+  });
+}
+
+function loadMinigame(gameFunc) {
+  // load minigame from minigames file
+}
+
+// utility functions ///////////////////////////////////////////////////////////
+function clearScreen() {
+  d_div.replaceChildren();
+  m_div.replaceChildren();
+  c_div.replaceChildren();
+}
+
+function createDialogue(dialogue) {
+  const div = document.createElement("div");
+  div.classList.add('dialogue');
+  div.innerHTML = `
+    <p>
+      ${GAME_SETTINGS.savedNames[dialogue.narrator] ?? dialogue.narrator}: 
+      ${dialogue.text}
+    </p>
+  `;
+  return div;
+}
+
+function createChoice(choice) {
+  const div = document.createElement("div");
+  div.classList.add('choice');
+  div.innerHTML = `
+    <p>${choice.text}</p>
+  `;
+  div.addEventListener("click", () => {
+    loadScene(choice.link);
+  });
+  return div;
+}
