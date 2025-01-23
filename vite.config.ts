b20ed2b41adf49db27b5b8115b96e43e7bef96e0
Lyssa Li <lyssa.li137@gmail.com>
2025-01-23 11:11:07 -0800

    autopublish attempt again again again

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/unfinished-game-project",
+  base: "https://maozblan.github.io/unfinished-game-project/",
 })
